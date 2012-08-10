dad60bcafec953e3f3bfa85c2c9ad88734cef523
jacob <jacob@Beagle-2.local>
2012-08-10 02:41:25 -0700

    adding data experiments page

--- js/charts/histogramChart.js
+++ js/charts/histogramChart.js
@@ -1,6 +1,15 @@
-define([], function(){
 
-  return function (opts) {
+
+var coursera_age_data = ["16", "21", "22", "20", "66", "66", "26", "17", "44", "45", "54", "34", "27", "53", "37", "40", "20", "27", "45", "40", "24", "59", "46", "59", "15", "21", "18", "23", "15", "26", "59", "23", "21", "36", "29", "20", "33", "29", "20", "18", "15", "20", "61", "61", "57", "15", "49", "36", "38", "16", "20", "40", "28", "49", "47", "70", "29", "18", "23", "16", "53", "40", "16", "69", "25", "21", "15", "24", "39", "27", "30", "19", "59", "46", "59", "15", "21", "18", "23", "15", "26", "59", "23", "21", "36", "29", "20", "33", "29", "20", "16", "46", "26", "19", "25", "18", "25", "27", "19", "20", "19", "19", "65", "31", "21", "22", "46", "23", "19", "51", "18", "19", "19", "49", "59", "46", "59", "15", "21", "18", "23", "15", "26", "59", "23", "21", "36", "29", "20", "33", "29", "20", "16", "24", "45", "16", "39", "71", "22", "32", "26", "15", "31", "38", "62", "31", "29", "27", "16", "24", "31", "24", "22", "19", "36", "22", "16", "59", "46", "59", "15", "21", "18", "23", "15", "26", "59", "23", "21", "36", "29", "20", "33", "29", "20", "24", "16", "22", "29", "20", "59", "46", "59", "15", "21", "18", "23", "15", "26", "59", "23", "21", "36", "29", "20", "33", "29", "20"];
+var opts = {
+    el: "#coursera-age-histogram",
+    data: coursera_age_data,
+    bins: [0,12,18,24,30,40,50,60,70,80,90]
+};
+
+histogram(opts);
+
+function histogram(opts) {
     opts = _.extend(
       {
         margin: {
@@ -46,10 +55,10 @@ define([], function(){
       .append("g")
         .attr("transform", "translate(" + opts.margin.left + "," + opts.margin.top + ")");
 
-    var bar = svg.selectAll(".bar")
+    var bar = svg.selectAll(".histogram-bar")
         .data(hist_data)
       .enter().append("g")
-        .attr("class", "bar")
+        .attr("class", "histogram-bar")
         .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
     bar.append("rect")
@@ -57,13 +66,13 @@ define([], function(){
         .attr("height", function(d) { return chart_height - y(d.y); });
 
     svg.append("g")
-        .attr("class", "x axis")
+        .attr("class", "histogram-axis")
         .attr("transform", "translate(0," + chart_height + ")")
         .call(xAxis);
 
     svg.append("g")
-        .attr('class', 'y axis')
+        .attr('class', 'histogram-axis')
         .attr('transform', 'translate(0,0)')
         .call(yAxis);
 
-}});
+};
